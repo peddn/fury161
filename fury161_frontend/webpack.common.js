ad3484342390e865681cad6fabee1d072f8d6d58
peddn <public@peddn.net>
2020-12-18 19:12:22 +0100

    integrated bulma and webpack

---
+++ fury161_frontend/webpack.common.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, '..', 'fury161', 'fury161', 'static', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '../css/bundle.css'
+    })
+  ]
+};
